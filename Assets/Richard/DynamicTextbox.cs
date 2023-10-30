23b98ca047365894cbe922dc18cba3f5ad399c21
ToTheCosmos <tothecosmosgamedevelopment@gmail.com>
2023-10-29 20:20:04 -0400

    Updated Dialogue System
    Removed Skipping Through Dialogue
    Sped Up Dialogue
    Dialogue waits when needing interaction from the player.

--- Assets/Richard/DynamicTextbox.cs
+++ Assets/Richard/DynamicTextbox.cs
@@ -6,10 +6,13 @@ using TMPro;
 public class DynamicTextbox : MonoBehaviour
 {
     public TextMeshProUGUI textComponent;
+    private string PlayerOption;
+    public string InteractiveString = "-";
 
     [TextArea(3, 10)]
     public string[] lines;
     public float textSpeed;
+    public bool Waiting = false;
 
     private int index;
 
@@ -29,31 +32,44 @@ public class DynamicTextbox : MonoBehaviour
             {
                 NextLine();
             }
-            else
-            {
-                StopAllCoroutines();
-                textComponent.text = lines[index];
-            }
         }
     }
 
     void StartDialogue()
     {
+        //Sets Index to 0 to start text
         index = 0;
         StartCoroutine(TypeLine());
     }
 
     IEnumerator TypeLine()
     {
+        //Types out each individual letter
+        //Need to make game wait for player to click an option
+        //If waiting for a response, stop typing
+        //When response is clicked, add it to current text line.
+        //Continue typing
         foreach (char c in lines[index].ToCharArray())
         {
-            textComponent.text += c;
-            yield return new WaitForSeconds(textSpeed);
+            if (Waiting == false)
+            {
+                textComponent.text += c;
+                yield return new WaitForSeconds(textSpeed);
+                if (c == InteractiveString[0])
+                {
+                    Waiting = true;
+                }
+            }
+            else
+            {
+
+            }
         }
     }
 
     void NextLine()
     {
+        //Moves to next line
         if (index < lines.Length - 1)
         {
             index++;
