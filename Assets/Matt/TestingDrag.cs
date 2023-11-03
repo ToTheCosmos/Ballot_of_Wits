e02ad7aeb74c9d47b5205ecdab61ba0ad50c17a4
ToTheCosmos <tothecosmosgamedevelopment@gmail.com>
2023-11-03 19:23:53 -0400

    Drag and Drop
    Dragging and Dropping answers with the attached script into object with triggers works, going to set up dialogue and essentially make the scene look how it is supposed to next.

---
+++ Assets/Matt/TestingDrag.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingDrag : MonoBehaviour
+{
+    private float mZCoord;
+    private Vector3 mOffset;
+    private Vector3 initialCoord;
+    private bool inTrigger = false;
+
+    private void Start()
+    {
+        initialCoord = gameObject.transform.position;
+    }
+    void OnMouseDown()
+    {
+        mZCoord = Camera.main.WorldToScreenPoint(
+        gameObject.transform.position).z;
+        // Store offset = gameobject world pos - mouse world pos
+        mOffset = gameObject.transform.position - GetMouseAsWorldPoint();
+    }
+
+    private Vector3 GetMouseAsWorldPoint()
+    {
+        // Pixel coordinates of mouse (x,y)
+        Vector3 mousePoint = Input.mousePosition;
+        // z coordinate of game object on screen
+        mousePoint.z = mZCoord;
+        // Convert it to world points
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    void OnMouseDrag()
+    {
+        transform.position = GetMouseAsWorldPoint() + mOffset;
+    }
+
+    private void OnMouseUp()
+    {
+        if (inTrigger==false)
+        {
+            gameObject.transform.position = initialCoord;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        inTrigger = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        inTrigger = false;
+    }
+}
